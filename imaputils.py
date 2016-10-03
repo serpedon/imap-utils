9407f97c2ab55a4153d25a9efabeb11a1dff2408
Michael Walz <code@serpedon.de>
2016-10-03 13:54:25 +0200

    Added new functionality for backuping
    - Added function backup_imap
    - Renamed function store_imap -> store_imap_to_mbox
    - Added additional functionality to scan_imap
      (parameters return_only_headers and mailbox_name)

--- imaputils.py
+++ imaputils.py
@@ -2,7 +2,7 @@
 # This work is licensed under GPL 3, see LICENSE
 # Author: Michael Walz <code@serpedon.de>, © 2016
 
-__all__ = ['scan_imap', 'store_imap' ]
+__all__ = ['scan_imap', 'store_imap_to_mbox', 'backup_imap']
 
 import imaplib
 import email
@@ -16,8 +16,11 @@ def parse_list_response(line):
     return (flags, delimiter, mailbox_name)
 
 
-def scan_imap(imap4, imap_search, store_command = None, return_found_msg = True) : 
+def scan_imap(imap4, imap_search, store_command = None, return_found_msg = True, return_only_headers = True, mailbox_name = None) : 
     """
+        The method scannes an imap-mailbox for messages.
+
+        Parameters: 
         imap4 is an IMAP4-instance with performed login.
             e.g. imap4 = imaplib.IMAP4_SSL("imap.example.com", 993)
                  imap4.login("username","password")
@@ -31,14 +34,24 @@ def scan_imap(imap4, imap_search, store_command = None, return_found_msg = True)
             e.g. store_command = ('+FLAGS', '\\Flagged') # to flag all found messages
 
         If return_found_msg == True, the function returns a list of all messages which match imap_search
-            each message is represented by a dictionary with keys: Folder, Id, From, To, Subject, Date
+            each message is represented by a dictionary with keys: 'Folder', 'Id', 'From', 'To', 'Subject', 'Date'
+    
+        If return_only_headers == True, the returned message contains only the headers (default).
+        If return_only_headers == False, the full messages is also returned via the key 'Message'
+        (return_only_headers is only evaluated when return_found_msg == True is set)
+
+        mailbox_name is the name of an imap folder which can be given to imap4.select(mailbox_name).
+        If set to None (default), the scan is performed for all imap folders.
     """
 
     foundMsg = []
 
-    result, mailbox_list = imap4.list()
-    if not result == 'OK' : raise RuntimeError('imap4.list(): ' + result) 
-
+    if mailbox_name is not None :
+        mailbox_list = [ b'() "." ' + mailbox_name.encode('utf-8') ]
+    else :
+        result, mailbox_list = imap4.list()
+        if not result == 'OK' : raise RuntimeError('imap4.list(): ' + result) 
+    
     for mailbox in mailbox_list:
         (flags, delimiter, mailbox_name) = parse_list_response(mailbox.decode('utf-8'))
 
@@ -54,17 +67,132 @@ def scan_imap(imap4, imap_search, store_command = None, return_found_msg = True)
                 if not result == 'OK' : raise RuntimeError('imap4.uid(store, ' + str(num) + ', ' + store_command + '): ' + result) 
 
             if return_found_msg :
-                result, data = imap4.uid('fetch', num, '(BODY[HEADER])') # '(RFC822)' would load the whole message
+                result, data = imap4.uid('fetch', num, '(BODY[HEADER])' if return_only_headers else '(RFC822)') # '(BODY[HEADER])' reads only the headers; '(RFC822)' loads the whole message
                 if not result == 'OK' : raise RuntimeError("imap4.uid(fetch, ...) in " + mailbox_name + '): ' + result) 
 
                 email_message = email.message_from_bytes(data[0][1])
-                foundMsg.append( dict(Folder=mailbox_name, Id=email_message['Message-ID'], From=email_message['From'], To=email_message['To'], Subject=email_message['Subject'], Date=email_message['Date']) )
+                Message = None if return_only_headers else email_message
+                foundMsg.append( dict(Folder=mailbox_name, Id=email_message['Message-ID'], From=email_message['From'], To=email_message['To'], Subject=email_message['Subject'], Date=email_message['Date'], Message=Message) )
 
     if return_found_msg :
         return foundMsg
 
+def backup_imap(imap4, backup_folder, deleted_folder = '_deleted') :
+    """
+        The method backups an imap-mailbox to local disk.
 
-def store_imap(imap4, directory) : 
+        Parameters: 
+        imap4 is an IMAP4-instance with performed login.
+            e.g. imap4 = imaplib.IMAP4_SSL("imap.example.com", 993)
+                 imap4.login("username","password")
+                 (after usage, imap4.close(); imap4.logout() is closed the connection)
+
+        backup_folder is a folder on the local disk where the imap-mailbox shall be backuped to.
+        Incremental backups are possible.
+        
+        deleted_folder gives the name of the subfolder were messages that already exist on the local 
+        disk but were deleted on the imap-server are moved to.
+    """
+
+    # imports
+    import os
+
+    # helper functions
+    def gen_filename(mail_dict) :
+        """
+            Generates a filename from a dictionary with keys 'Id', 'Date', 'Folder'
+        """
+        from email.utils import parsedate_to_datetime
+        dateObj = parsedate_to_datetime(mail_dict['Date'])
+        fulldate = dateObj.strftime("%Y-%m-%m_%H.%M_utc%z")
+        year = dateObj.strftime("%Y")
+
+        Id = mail_dict['Id'].replace('<', '').replace('>', '').replace('%', '')
+
+        Folder = mail_dict['Folder'].replace(' ', '_')
+
+        return os.path.join(Folder, year, fulldate + "_" + Id + '.eml')
+
+    def create_dir_if_not_exist(folder) :
+        if not os.path.exists(folder) :
+            os.makedirs(folder)
+    
+    # Search for all messages that are not deleted
+    allMsgHeaders = scan_imap(imap4, imap_search="(Undeleted)")
+
+    msg_already_existing = 0
+    msg_downloaded = 0
+
+    paths_of_all_msg = set()
+
+    # Download new messages (if not available locally)
+    for msg in allMsgHeaders:
+        from email import generator
+        filename = gen_filename(msg)
+        full_path = os.path.join(backup_folder, filename)
+        directory = os.path.dirname(full_path)
+        paths_of_all_msg |= { full_path }
+        create_dir_if_not_exist(directory)
+        if os.path.exists(full_path) :
+            msg_already_existing += 1
+        else :
+            msg_downloaded += 1
+            print('%s is new own server --> downloading' % (filename))
+            full_msgs = scan_imap(imap4, imap_search="(Header Message-ID " + msg['Id'] + ")", return_only_headers = False, mailbox_name = msg['Folder'])
+            for full_msg in full_msgs :
+                filename2 = gen_filename(full_msg)
+                if not (filename == filename2) : raise RuntimeError('Error: filenames do not match: ' + filename + ' ; ' + filename2) 
+
+                with open(full_path, 'w') as outfile:
+                    gen = generator.Generator(outfile)
+                    gen.flatten(full_msg['Message'])
+                break
+
+
+    # Update local list of paths of all messages with all parent directories
+    for path in list(paths_of_all_msg) :
+        dirname = path
+        old_dirname = None
+        while dirname != old_dirname :
+            old_dirname = dirname
+            dirname = os.path.dirname(dirname)
+            paths_of_all_msg |= { dirname }
+
+    # Move all file in 'deleted'-subdirectories which do not exist on the imap-server anymore
+    msg_keeping = 0
+    msg_move_to_deleted = 0
+    def move_deleted_messages(folder) :
+        nonlocal msg_keeping
+        nonlocal msg_move_to_deleted
+        for filename in os.listdir(folder) :
+            if filename != deleted_folder :
+                full_path = os.path.join(folder, filename)
+                if full_path in paths_of_all_msg :
+                    if not os.path.isdir(full_path) : msg_keeping += 1
+                    #print(filename + " present")
+                else :
+                    if not os.path.isdir(full_path) : msg_move_to_deleted += 1
+                    parent_dir = os.path.dirname(full_path)
+                    deleted_dir = os.path.join(parent_dir, deleted_folder)
+                    create_dir_if_not_exist(deleted_dir)
+                    print('%s was removed from server --> moving to %s' % (full_path[(len(backup_folder)+1):], deleted_folder))
+                    os.rename(full_path, os.path.join(deleted_dir, filename))
+                
+                if os.path.isdir(full_path) :
+                    move_deleted_messages(full_path)
+
+    move_deleted_messages(backup_folder)
+
+    print("Summary (downloads):")
+    print("  A=%d messages have been downloaded in the current run" % msg_downloaded)
+    print("  B=%d messages have not been downloaded because they already existed locally" % msg_already_existing)
+    print("Summary (local disc):")
+    print("  C=%d messages are locally present which are also present on the server" % msg_keeping)
+    print("  D=%d messages were moved to '%s'-directory because they are not present on server anymore" % (msg_move_to_deleted, deleted_folder))
+    print("Consistency check: A+B==C; %d+%d==%d; %s" % (msg_downloaded,msg_already_existing,msg_keeping, 'SUCCESS' if msg_downloaded+msg_already_existing==msg_keeping else 'FAILED'))
+
+
+def store_imap_to_mbox(imap4, directory) : 
     result, mailbox_list = imap4.list()
     if not result == 'OK' : raise RuntimeError('imap4.list(): ' + result) 
 
